a88c24e5995c9fd405cca7d8bae4d4201d141c7d
edelciomolina <edelciomolina@gmail.com>
2024-09-23 16:50:43 -0300

    Add: Tests for dump and restore with verbose args

--- tests/main.test.js
+++ tests/main.test.js
@@ -7,40 +7,62 @@ describe("Can dump and restore database", () => {
     await database.sequelize.sync({ force: true });
     await database.populate();
   });
-  describe("Can dump", () => {
-    test("can dump database", async () => {
-      expect.assertions(1);
-      await pgDumpRestore.dump({
-        ...database.CREDENTIALS,
-        file: "./test.pgdump",
-      });
-      expect(await fs.pathExists("./test.pgdump")).toBeTruthy();
+  test("can dump database", async () => {
+
+    const log = await pgDumpRestore.dump({
+      ...database.CREDENTIALS,
+      file: "./test.pgdump",
     });
+
+    expect(await fs.pathExists("./test.pgdump")).toBeTruthy();
+    expect(log.stderr.includes('pg_dump:')).toBeFalsy();
+
   });
-  describe("Can dump (with verbose)", () => {
-    test("can dump database", async () => {
-      expect.assertions(1);
-      await pgDumpRestore.dump({
-        ...database.CREDENTIALS,
-        verbose: true,
-        file: "./test.pgdump",
-      });
-      expect(await fs.pathExists("./test.pgdump")).toBeTruthy();
+  test("can restore database", async () => {
+
+    await database.sequelize.drop();
+    const log = await pgDumpRestore.restore({
+      ...database.CREDENTIALS,
+      filename: "./test.pgdump",
     });
-  });
-  describe("Can restore", () => {
-    test("can restore database", async () => {
-      expect.assertions(1);
-      await database.sequelize.drop();
-      await pgDumpRestore.restore({
-        ...database.CREDENTIALS,
-        filename: "./test.pgdump",
-      });
-      allPatients = await database.Patient.findAll();
-      expect(allPatients.length).toBe(1);
-    });
-  });
-  afterAll(async () => {
-    await fs.remove("./test.pgdump");
+    allPatients = await database.Patient.findAll();
+    expect(allPatients.length).toBe(1);
+    expect(await fs.pathExists("./test.pgdump")).toBeTruthy();
+    expect(log.stderr.includes('pg_restore:')).toBeFalsy();
   });
 });
+describe("Can dump and restore database (with verbose)", () => {
+  beforeAll(async () => {
+    await database.sequelize.sync({ force: true });
+    await database.populate();
+  });
+  test("can dump database (With verbose)", async () => {
+
+    const log = await pgDumpRestore.dump({
+      ...database.CREDENTIALS,
+      verbose: true,
+      file: "./test.pgdump",
+    });
+
+    expect(await fs.pathExists("./test.pgdump")).toBeTruthy();
+    expect(log.stderr.includes('pg_dump:')).toBeTruthy();
+
+  });
+
+  test("can restore database (with verbose)", async () => {
+
+    await database.sequelize.drop();
+    const log = await pgDumpRestore.restore({
+      ...database.CREDENTIALS,
+      verbose: true,
+      filename: "./test.pgdump",
+    });
+    allPatients = await database.Patient.findAll();
+    expect(allPatients.length).toBe(1);
+    expect(await fs.pathExists("./test.pgdump")).toBeTruthy();
+    expect(log.stderr.includes('pg_restore:')).toBeTruthy();
+  });
+});
+afterAll(async () => {
+  await fs.remove("./test.pgdump");
+}); 
